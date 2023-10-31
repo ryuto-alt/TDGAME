f7fe160bc6c28c8f4cb7ff2478aceb9b25e7e645
uno_ryuto <k023g0078@g.neec.ac.jp>
2023-10-31 21:46:52 +0900

    シーン切り替え

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <Novice.h>
+#include"audio.h"
+#include"easing.h"
+#include<Vector2.h>
+
 
 const char kWindowTitle[] = "TDGAME";
+
+
+//イージング用ラープ関数　※いじらないで
+Vector2 leap(const Vector2& start, const Vector2& end, float t) {
+	Vector2 ret;
+	ret.x = start.x + (end.x - start.x) * t;
+	ret.y = start.y + (end.y - start.y) * t;
+	return ret;
+}
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -8,9 +22,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	//=======フラグ=======
+	int sceneCount = 0;
+
+	//=======フラグ=======
+	
+	//=======グラフハンドル=======
+	 
+	
+	//=======グラフハンドル=======
+	
+
+	//=======Audioハンドル=======
+	 
+	
+	//=======Audioハンドル=======
+
+
+
+	enum scene {
+		title = 0,
+		exp = 1,
+		play = 2,
+		result = 3
+	};
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -23,8 +62,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		///
 		/// ↓更新処理ここから
-		///
 
+		// シーン描画
+		switch (sceneCount) {
+		case title :
+
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+				sceneCount += 1;
+			}
+			break;
+		case exp://ルール説明
+
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+				sceneCount += 1;
+			}
+			break;
+		case play:
+
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+				sceneCount += 1;
+			}
+			break;
+		case result:
+
+			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+				sceneCount += 1;
+			}
+			break;
+		}
 		///
 		/// ↑更新処理ここまで
 		///
@@ -37,6 +102,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↑描画処理ここまで
 		///
 
+
+
+
+		Novice::ScreenPrintf(0, 0, "%d", sceneCount);
 		// フレームの終了
 		Novice::EndFrame();
 
